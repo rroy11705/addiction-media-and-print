d81684d6cfef027a1f887d7b89e9f36b962d7497
rroy11705 <rroy11705@gmail.com>
2023-08-30 12:52:48 +0530

    connection fix

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
-$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/addiction-media-and-print/');
+$dev_data = array('id'=>'-1','firstname'=>'Rahul','lastname'=>'Roy','username'=>'bvibemurvoumarbvhmvme974g7gat','password'=>'34e6808856fcaf524d6a4386059d8cec','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','https://addictionmedia.zerodollarsecurity.in/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"db_addiction_media_and_print");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"zerodbu8_addictionmedia_user");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"otnDoRR1gK");
+if(!defined('DB_NAME')) define('DB_NAME',"zerodbu8_addictionmedia");
 ?>
